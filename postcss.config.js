c2e578b26a4c6011cb62f2484f8e2a9a5824dd86
Ricardo Hernandez <ricardo.hernandez@SECPER.COM>
2019-08-13 02:09:47 -0500

    add purgecss

---
+++ postcss.config.js
@@ -0,0 +1,31 @@
+/*
+module.exports = {
+    plugins: [
+        require('postcss-import'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                "./client/index.html"
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
+    ]
+}
+*/
+/**
+ *
+ *
+"postcss": {
+    "plugins": {
+      "postcss-import": {},
+      "tailwindcss": {},
+      "autoprefixer": {},
+      "@fullhuman/postcss-purgecss":{
+        "content":[
+          "./client/index.html"
+        ]
+      }
+    }
+  }
+ */
